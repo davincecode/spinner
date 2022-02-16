dc8057cbc8460a47ab2efa355523ffd62961c7e2
davincecode <hyperscript.ca@gmail.com>
2022-02-16 00:26:01 +0000

    cleaned up code

--- spinner2.js
+++ spinner2.js
@@ -1,12 +1,9 @@
 let sentence = "\r|/-|-/|/-|-/|/-|-/|/-|-/|";
 let timer = 0;
-let result = [];
 for (const char of sentence) {
   setTimeout(() => {
     process.stdout.write(char);
     process.stdout.cursorTo(0);
-    // process.stdout.clearLine(); to clear the line
   }, timer);
   timer += 200;
 }
-return result;
